def9d3e9dc9719dc4f1961691eea212f43da826f
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-05-31 19:54:36 -0300

    server js (node concepts)

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    res.end("<h1>Entrouuu</h1>")
+});
+
+server.listen(3000, () => {
+    console.log("actived server");
+});
